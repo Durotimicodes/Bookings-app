d0aeab7c9c3788665b30adcecad6b4120a29ba95
admin <admin@admins-MacBook-Pro.local>
2022-09-19 18:40:41 +0100

    addding images

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -29,14 +29,23 @@ func NewHandlers(r *Repository) {
 }
 
 func (R *Repository) HomePage(w http.ResponseWriter, r *http.Request) {
+
+	/*everytime a client makes a request to this page this will store the
+	clients Ip address (IPv4 or IPv6)*/
+	remoteIP := r.RemoteAddr
+	R.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.html", &models.TemplateData{})
 
 }
 
 func (R *Repository) AboutPage(w http.ResponseWriter, r *http.Request) {
 	sm := make(map[string]string)
-
 	sm["test"] = "Hello monolithic application"
+
+	//grab the session value gotten from the HomePage
+	remoteIP := R.App.Session.GetString(r.Context(), "remote_ip")
+	sm["remote_ip"] = remoteIP
 
 	//send data to template
 	render.RenderTemplate(w, "about.html", &models.TemplateData{
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Bookings and Reservations
+
+This is the repository for my bookings and reservations project.
+
+- Built in Go version 1.15
+- Uses the [gochi router](https://github.com/go-chi/chi)
+- Uses the alexedward SCS session management
+- Uses [nosurf](https://github.com/justinas/nosurf])
